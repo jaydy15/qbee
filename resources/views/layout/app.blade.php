80ed9168080af30b565f9fa5bf00b4bd2fbf0dc8
joshua <joshuaadrian46@gmail.com>
2019-03-24 15:59:08 +0800

    Layout the question as a form

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,12 +11,12 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     {{-- <link href="https://bootswatch.com/4/superhero/bootstrap.min.css" rel="stylesheet"> --}}
     {{--  <link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css">  --}}
     <script src="https://code.jquery.com/jquery-latest.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+    {{--  <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">  --}}
 </head>
 <body>
 
