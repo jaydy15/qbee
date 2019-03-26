b2b624035a977ff404860a2db47b5cbe71204dd8
joshua paboroquez <joshuaadrian46@gamil.com>
2019-03-26 13:15:03 +0800

    border color

--- resources/views/lobby/index.blade.php
+++ resources/views/lobby/index.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="container">
-<div class="jumbotron jumbotron-fluid m-5">
+<div class="jumbotron jumbotron-fluid border border-danger m-5">
     <div class="container">
         <h3>Enter this game pin to join:</h3>
     </div>
   <div class="container">
-    <h1 class="display-4 font-weight-bold text-center">{{$quiz->game_pin}}</h1>
+    <h1 class="display-4 font-weight-bold text-center ">{{$quiz->game_pin}}</h1>
   </div>
 </div>
 </div>
