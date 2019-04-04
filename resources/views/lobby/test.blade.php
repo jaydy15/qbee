a5fa38f777b5d2f1d669b415e5d8baa338879ea9
joshua paboroquez <joshuaadrian46@gamil.com>
2019-04-04 14:52:10 +0800

    layout the test.blade.php

--- resources/views/lobby/test.blade.php
+++ resources/views/lobby/test.blade.php
@@ -15,8 +15,48 @@
                     <div class="col-md-8 col-sm-8">
                         <h3><a>{{$question->question}}</a></h3>
                         <div class="text-right">
+                        <a href="/questions/{{$question->id}}/edit" class="btn btn-primary">Edit</a>
+                        <a href="/questions/{{$question->id}}/edit" class="btn btn-primary">Skip</a>
+
                         </div>
-                        <small>Category on {{$question->question_type}}</small>
+                        <!-- Question Type -->
+                            @if (($question->question_type) == 'mc' || ($question->question_type) == 'a')
+                        <div class="container">
+                            <small>Question Type: Multiple Choice</small> 
+                            <!-- correct answer -->
+                                @if(($question->right_answer)== 1)
+                                    <small class="pl-2 ml-2" style="font-weight:bold">Correct Answer:{{$question->choice1}}</small>
+
+                                @elseif(($question->right_answer)== 2)
+                                    <small class="pl-2 ml-2" style="font-weight:bold">Correct Answer:{{$question->choice2}}</small>
+
+                                @elseif(($question->right_answer)== 3)
+                                    <small class="pl-2 ml-2" style="font-weight:bold">Correct Answer:{{$question->choice3}}</small>
+
+                                @elseif(($question->right_answer)== 4)
+                                    <small class="pl-2 ml-2" style="font-weight:bold">Correct Answer:{{$question->choice4}}</small>
+
+                                @endif
+                            <!-- end of correct answer -->
+                            @elseif (($question->question_type) == 'tf' || ($question->question_type) == 'b')
+                            <div class="container">
+                            <small>Question Type: True or False</small>
+                            <!-- correct answer --> 
+                                @if(($question->true_false)== 1)
+                                        <small class="pl-2 ml-2" style="font-weight:bold">Correct Answer: True</small>
+                                @elseif(($question->true_false)== 0)
+                                        <small class="pl-2 ml-2" style="font-weight:bold">Correct Answer: False</small>
+                                @endif
+                            </div>
+                            <!-- correct answer --> 
+                            @elseif (($question->question_type) == 'shan' || ($question->question_type) == 'c') 
+                            <div class="container">
+                                <small>Question Type:Short Answer</small> 
+                                <small class="pl-2 ml-2" style="font-weight:bold">Correct Answer:{{$question->short_answer}}</small>  
+                            </div>
+                            @endif
+                        </div> 
+                        <!-- End of Question Type -->
                     </div>
                 </div>
             </div>
